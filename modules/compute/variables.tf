effd14bd471555107bea87d66e62f8bfc88876e4
geomux <Henry.LawrenceGEO@gmail.com>
2026-08-15 15:08:09 -0700

    feat(/compute/variables.tf): created deifnition blocks for all variables references in the relative main.tf.

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,2 +1,24 @@
 # modules/compute/variables.tf
 # Variables configuration for compute module.
+
+variable "instance_type" {
+  description = "The type of instance to create."
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ebs_volume_size" {
+  description = "The size of the EBS volume in GB."
+  type        = number
+  default     = 8
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet where the instance will be launched."
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "A list of security group IDs to associate with the instance."
+  type        = list(string)
+}
