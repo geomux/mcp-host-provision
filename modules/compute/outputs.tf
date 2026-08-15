da02bd038d1ac28deda6adcf6f70d035b9d48b8b
geomux <Henry.LawrenceGEO@gmail.com>
2026-08-15 15:07:12 -0700

    feat(/compute/outputs.tf): created definitions for Instance and Volume outputs. EC2 instance is the MCP server Host machine. EBS volume is the storage for that Host machine.

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,2 +1,12 @@
 # modules/compute/outputs.tf
 # Outputs definitions for compute module.
+
+output "mcp_server_host" {
+  description = "The EC2 instance created to hold the remote MCP server."
+  value       = aws_instance.mcp_ec2_instance.id
+}
+
+output "mcp_server_storage" {
+  description = "The EBS volume created to hold the remote MCP server storage."
+  value       = aws_ebs_volume.mcp_ebs_volume.id
+}
