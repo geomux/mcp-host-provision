71043344d20b9679f8e4281de71db3a1fda37d7f
geomux <Henry.LawrenceGEO@gmail.com>
2026-08-16 13:06:02 -0700

    feat(/networking/outputs.tf): added outputs to pass as variables in root main.tf to the compute module.

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,2 +1,17 @@
 # modules/network/outputs.tf
 # Outputs definitions for networking module.
+
+output "vpc_id" {
+  description = "The ID of the created VPC."
+  value       = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  description = "The ID of the created subnet."
+  value       = aws_subnet.main.id
+}
+
+output "security_group_id" {
+  description = "The ID of the created security group."
+  value       = aws_security_group.allow_ssh_https.id
+}
