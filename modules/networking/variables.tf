e5133575b6b7f9c487ba796aa622834239205845
geomux <Henry.LawrenceGEO@gmail.com>
2026-08-16 13:10:22 -0700

    feat(/networking/variables.tf): added variable for aws_region, optional flexibility for those using this repo outside of North America/east.

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,2 +1,27 @@
 # modules/networking/variables.tf
 # Variables configuration for networking module.
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block for the subnet."
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_az" {
+  description = "The availability zone for the subnet."
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "allowed_ssh_cidr" {
+  description = "The CIDR block allowed to access the VPC via SSH."
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
