87c83a2afce150425d330dba6115bad76eb6e4d8
geomux <Henry.LawrenceGEO@gmail.com>
2026-08-16 13:20:06 -0700

    feat(/networking/main.tf): added main and auxiliary cloud infra resource blocks. Added VPC, Internet Gateway, Route Table, Subnet, Security Group- MAIN infra. Added route table association, security group ingress/egress rules- AUXILIARY infra.

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,2 +1,94 @@
 # modules/networking/main.tf
 # Main config file for the networking module.
+
+
+### ------------------------
+### --- MAIN CLOUD INFRA ---
+### ------------------------
+
+
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Main VPC"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Main IGW"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "Main Route Table"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.subnet_az
+
+  tags = {
+    Name = "Main Subnet"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_https" {
+  name        = "allow_ssh_https"
+  description = "Allow SSH and HTTPS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "Allow SSH and HTTPS Security Group"
+  }
+}
+
+
+### -----------------------------
+### --- AUXILIARY CLOUD INFRA ---
+### -----------------------------
+
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_ssh_https.id
+
+  cidr_ipv4   = var.allowed_ssh_cidr
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.allow_ssh_https.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.allow_ssh_https.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1" # '-1' means all protocols
+}
