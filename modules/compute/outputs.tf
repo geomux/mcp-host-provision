014a77fe6d5e5054fe7d4d3c1b0f6f7824c89576
geomux <Henry.LawrenceGEO@gmail.com>
2026-08-16 20:52:52 -0700

    feat: Added logic to produce a public facing IPv4 on the mcp server instance and output it in root terraform config. /networking/main.tf aws_subnet resource now has a map_public_ip_on_launch = true so any instance created within it gets a public facing IPv4. /compute/outputs.tf now has an output value for the mcp_server_public_ip that can be forwarded to the root outputstf. root outputs.tf now has actual outputs, instance ID, storage ID, and the public IPv4.

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -10,3 +10,9 @@ output "mcp_server_storage" {
   description = "The EBS volume created to hold the remote MCP server storage."
   value       = aws_ebs_volume.mcp_ebs_volume.id
 }
+
+output "mcp_server_public_ip" {
+  description = "The public IPv4 address auto-assigned to the instance at launch."
+  value       = aws_instance.mcp_ec2_instance.public_ip
+}
+
--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,18 @@
 # outputs.tf
-# Root Terraform outputs config file for this IaC repo. Holds provisioned resource values after apply is run.
+# Root Terraform outputs config file for this IaC repo. 
+# Defines provisioned resource values shown after Plan/Apply is run.
+
+output "mcp_server_public_ip" {
+  description = "The public IPv4 address auto-assigned to the instance at launch."
+  value       = module.compute.mcp_server_public_ip
+}
+
+output "mcp_server_host" {
+  description = "The EC2 instance created to hold the remote MCP server."
+  value       = module.compute.mcp_server_host
+}
+
+output "mcp_server_storage" {
+  description = "The EBS volume created to hold the remote MCP server storage."
+  value       = module.compute.mcp_server_storage
+}
